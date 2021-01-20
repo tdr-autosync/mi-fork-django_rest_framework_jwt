9a1a51ac4025ae57cf526c4ed5cafdcac52eec28
Ashok Argent-Katwala <ashok@delphia.com>
2021-01-20 14:40:57 -0500

    Permit storing either the token or its original id (or both) for blacklist entries.
    For Django 2.2 and above, also add constraint that at least one of the values is non-null. Since we allow configuring whether token ids are included, the token_id column needs to be optional. Where we can rely on token ids, I am going to storing the whole token value, so that also needs to be nullable.

--- src/rest_framework_jwt/blacklist/models.py
+++ src/rest_framework_jwt/blacklist/models.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 
+from django import VERSION
 from django.conf import settings
 from django.db import models
+from django.db.models import Q
 from django.utils import timezone
 
 
@@ -11,7 +13,18 @@ class BlacklistedTokenManager(models.Manager):
 
 
 class BlacklistedToken(models.Model):
-    token = models.TextField(db_index=True)
+    class Meta:
+        if VERSION >= (2, 2):
+            constraints = [
+                models.CheckConstraint(
+                    check=Q(token_id__isnull=False) | Q(token__isnull=False),
+                    name='token_or_id_not_null',
+                )
+            ]
+
+    # This holds the original token id for refreshed tokens with ids
+    token_id = models.UUIDField(db_index=True, null=True)
+    token = models.TextField(db_index=True, null=True)
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     expires_at = models.DateTimeField(db_index=True)
